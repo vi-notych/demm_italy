9a371496036e89db2cff2827e3b0d8ebdcf7fb81
vi-notych <vin.arnet@gmail.com>
2024-10-23 11:39:11 +0300

    сделал секцию category

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ new Swiper('.slider__swiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
-});
+})
+
+const openPopUp = document.getElementById('open');
+const closePopUp = document.getElementById('close');
+const popUp = document.getElementById('popup');
+
+openPopUp.addEventListener('click', function (e) {
+  e.preventDefault();
+  popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () => {
+  popUp.classList.remove('active');
+})
